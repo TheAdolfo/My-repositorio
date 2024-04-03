01944075212499fa088c406f294aad1d78e608f8
Adolfo Mata <adolfomata.cemas@gmail.com>
2024-04-03 12:07:30 -0400

    login

--- Projecto/projecto web, suma y resta de 2.py
+++ Projecto/projecto web, suma y resta de 2.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def hello():
     return '<body bgcolor="pink"> <img src="https://www.clasesnet.com/clases/imagenes/logo.png" alt="logo de nuestra pagina web"> <center> <img src="https://josmorbor.neocities.org/img/boton_inicio.png" style="width: 155px; height: 59px; "><a href="/nosotros"><img src="https://grupodispersa.com.gt/wp-content/uploads/2021/07/boton-nosotros.png" style="width: 155px; height: 59px; "></a> <img src="https://aete.org/wp-content/uploads/2016/07/BotonesServiciosAgua.png" style="width: 155px; height: 55px; "><img src="https://multicon.com.mx/wp-content/uploads/2019/08/boton-contacto.png" style="width: 155px; height: 55px; "></center> <center> <h1 style="color: red;">Hello, world</h1></center><center><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ69MR2WkachtqqExQ1haf1E8kEq9j7agvKMujRDdR5A&s"></center> <center><p>Esto es un texto de prueba si, texto de prueba, texto de prueba, parrafo de prueba parrafo de prueba</p> </center> '
 
+@app.route('/login') #ruta 2
+def login():
+    return '<body bgcolor="white"><center> <h1 style="color: red;">Registrate</h1>   <br><form action="ejemplo.php" method="get"> <p>Nombre: <input type="text"  size="40"></p> <p>Contraseña: <input></p></br><a href="http://127.0.0.1:5000/"><button href>Iniciar sesion</button> </a></center> '
+
+
+
 @app.route('/nosotros') #ruta 2
 def nosotros():
     return '<body bgcolor="black"> <h1 style="color: red;">Hola, Esto es nosotros</h1>'
